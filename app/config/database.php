b378755a208f8493d496df899309b0394d865a84
shashankmurali <shashankmurali93@gmail.com>
2013-06-06 01:42:28 +0530

    Migrations and Seeding

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'tv_app_database',
 			'username'  => 'root',
-			'password'  => 'mamnsha29',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
