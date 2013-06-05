1795a58bdd70eae8e7899911a4f66a1b130323c2
shashankmurali <shashankmurali93@gmail.com>
2013-06-06 00:42:24 +0530

    Migrations

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'tv_app_database',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'mamnsha29',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
